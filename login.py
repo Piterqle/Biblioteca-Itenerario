7b13b6ed8a8ee9e2071e2db602727afe2985091d
Piterqle <pedrohenriquemendesmartins5@gmail.com>
2024-09-20 17:25:42 -0300

    Start BD

--- login.py
+++ login.py
@@ -22,7 +22,7 @@ class Login(tk.Tk):
         for widget in self.winfo_children():
             widget.destroy()
 
-        self.geometry("500x600")
+        self.geometry("500x700")
         self.title("Login na Biblioteca")
         
         frame_login = tk.Frame(self, width=400, height=300)
@@ -40,10 +40,10 @@ class Login(tk.Tk):
         self.txb_senha.pack(anchor="w")
 
         frame_buttom = tk.Frame(self)
-        frame_buttom.pack(pady=30)
-        login = tk.Button(frame_buttom, text="Entrar", font=("Arial", 10, "bold"), width=15, command=self.logar)
+        frame_buttom.pack(pady=100)
+        login = tk.Button(frame_buttom, text="Entrar", font=("Arial", 10, "bold"), width=20, command=self.logar)
         login.pack(pady= 10)
-        cancelar = tk.Button(frame_buttom, text="Cancelar", font=("Arial", 10, "bold"), width=15, command=lambda:self.sair(self))
+        cancelar = tk.Button(frame_buttom, text="Cancelar", font=("Arial", 10, "bold"), width=20, command=lambda:self.sair(self))
         cancelar.pack()
 
         frame_label = tk.Frame(self)
@@ -61,7 +61,7 @@ class Login(tk.Tk):
         for widget in self.winfo_children():
             widget.destroy()
         
-        frame_login = tk.Frame(self, width=400, height=450)
+        frame_login = tk.Frame(self, width=400, height=700)
         frame_login.pack(anchor="center")
         frame_login.propagate(False)
     
@@ -70,6 +70,10 @@ class Login(tk.Tk):
         tk.Label(frame_login, text="Nome", font=("Arial", 14)).pack(anchor="w", pady=15)
         self.txb_nomecadastro = tk.Entry(frame_login, width=66)
         self.txb_nomecadastro.pack(anchor="w")
+        
+        tk.Label(frame_login, text="Sobrenome", font=("Arial", 14)).pack(anchor="w", pady=15)
+        self.txb_snomecadastro = tk.Entry(frame_login, width=66)
+        self.txb_snomecadastro.pack(anchor="w")
 
         tk.Label(frame_login, text="Email", font=("Arial", 14)).pack(anchor="w", pady=15)
         self.txb_email = tk.Entry(frame_login, width=66)
@@ -83,27 +87,69 @@ class Login(tk.Tk):
         self.txb_confirmcadastro = tk.Entry(frame_login, width=66)
         self.txb_confirmcadastro.pack(anchor="w")
 
-        frame_buttom = tk.Frame(self)
-        frame_buttom.pack(pady=30)
-        cadastrar = tk.Button(frame_buttom, text="Entrar", font=("Arial", 10, "bold"), width=15, command=self.cadastrar)
-        cadastrar.pack(pady= 10)
+        tk.Label(frame_login, text="Gênero", font=("Arial", 14)).pack(anchor="w", pady=15)
+        frame_rb = tk.Frame(frame_login)
+        frame_rb.pack(anchor="w")
+        
+        self.rb_val = tk.IntVar(value=0)
+        rb_Masculino = tk.Radiobutton(frame_rb, variable=self.rb_val, value=1,text="Masculino", font=("Arial", 10))
+        rb_Masculino.grid(column=0, row=0)
+        rb_Feminino = tk.Radiobutton(frame_rb, variable=self.rb_val, value=2,text="Feminino", font=("Arial", 10))
+        rb_Feminino.grid(column=1, row=0)
+
+        frame_buttom = tk.Frame(frame_login)
+        frame_buttom.pack(pady=15)
+        cadastrar = tk.Button(frame_buttom, text="Cadastrar", font=("Arial", 10, "bold"), width=15, command=self.cadastrar)
+        cadastrar.grid(column=0, row=0, padx=10)
         cancelar = tk.Button(frame_buttom, text="Cancelar", font=("Arial", 10, "bold"), width=15, command=self.login_screen)
-        cancelar.pack()
+        cancelar.grid(column=1, row=0)
 
     def cadastrar(self):
         nome = self.txb_nomecadastro.get()
+        sbn = self.txb_snomecadastro.get()
+        genero = self.verificar_genero()
         email = self.txb_email.get()
         senha = self.txb_senhacadastro.get()
         senha_confirm= self.txb_confirmcadastro.get()
 
-        comando = "INSERT INTO dim_usuario (usuário, email) VALUES (%s, %s)"
-        val = (nome, email)
+        if genero != None:
+            self.add_sql(genero, "fato_genero", "genero")
+            messagebox.showinfo("Login Cadastrado", f"Gênero = {genero}")
+            self.cursor.execute("INSERT INTO `teste` (teste) VALUES `fato_genero`.id_genero where genero = 'Masculino'")
+            self.login_screen()
+            
 
-        self.cursor.execute(comando, val)
-        self.conexao.commit()
+        else:
+            messagebox.showerror("Erro", "Preencha os Espaços")
+
+            
     
     def sair(self, root):
         root.destroy()
+    
+    def verificar_genero(self):
+        valor = self.rb_val.get()
+        if valor == 1:
+            return "Masculino"
+        elif valor == 2:
+            return "Feminino"
+        else:
+            return None
+    
+    def add_sql(self, valor, tabela, coluna):
+        comando_selecionar = "SELECT id_genero FROM " + tabela + " where " + coluna + "= %s"
+        self.cursor.execute(comando_selecionar, (valor,))
+        resultado = self.cursor.fetchone()
+        if resultado:
+            pass
+        else:
+            comando_adicionar = "INSERT INTO `biblioteca itinerante`."+ f"`{tabela}`" + f"({coluna})" +" VALUES (%s);"
+            self.cursor.execute(comando_adicionar, (valor,))
+            self.conexao.commit()
+    
+    def adds_sql(self, valores, tabela, colunas):
+        pass
+        
         
 
 if __name__ == "__main__":
